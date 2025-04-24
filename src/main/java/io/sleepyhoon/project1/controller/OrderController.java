1cc4131bcd99ae8a071ea37c4fe3cd183b08a22c
chw0912 <gmldnd28@gmail.com>
2025-04-24 11:24:58 +0900

    Feat: 입력한 email에 해당하는 모든 주문 조회 구현

---
+++ src/main/java/io/sleepyhoon/project1/controller/OrderController.java
@@ -0,0 +1,31 @@
+package io.sleepyhoon.project1.controller;
+
+import io.sleepyhoon.project1.dto.ApiResponse;
+import io.sleepyhoon.project1.dto.OrderDto;
+import io.sleepyhoon.project1.entity.Order;
+import io.sleepyhoon.project1.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Order>>> getAllOrders(@RequestParam String email) {
+        List<Order> orders = orderService.findByEmailAllOrders(email);
+        return ResponseEntity.ok(new ApiResponse<>(orders, "조회 성공", HttpStatus.OK.value()));
+    }
+
+
+}
