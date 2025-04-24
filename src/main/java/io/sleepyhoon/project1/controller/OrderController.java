eef79686797766296469a0ebe0a4bb98f2fd4ae7
chw0912 <gmldnd28@gmail.com>
2025-04-24 13:43:22 +0900

    Feat: 입력한 주문 저장 구현

--- src/main/java/io/sleepyhoon/project1/controller/OrderController.java
+++ src/main/java/io/sleepyhoon/project1/controller/OrderController.java
@@ -7,10 +7,7 @@ import io.sleepyhoon.project1.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +21,12 @@ public class OrderController {
     @GetMapping
     public ResponseEntity<ApiResponse<List<Order>>> getAllOrders(@RequestParam String email) {
         List<Order> orders = orderService.findByEmailAllOrders(email);
-        return ResponseEntity.ok(new ApiResponse<>(orders, "조회 성공", HttpStatus.OK.value()));
+        return ResponseEntity.ok(new ApiResponse<>(orders, "조회 성공", 200));
     }
 
-
+    @PostMapping
+    public ResponseEntity<ApiResponse<Order>> saveOrder(@RequestBody OrderDto orderDto) {
+        Order order = orderService.save(orderDto);
+        return ResponseEntity.ok(new ApiResponse<>(order, "주문 성공", 201));
+    }
 }
