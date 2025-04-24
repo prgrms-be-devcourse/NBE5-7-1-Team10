8eba6fb81b73f37c2fbeb64bd80bd4589685fb2c
Moonsung Bae <qoanstjdsla@gmail.com>
2025-04-24 11:30:37 +0900

    feat: 중복이름 커피 검사 기능 구현

---
+++ src/main/java/io/sleepyhoon/project1/exception/CoffeeDuplicationException.java
@@ -0,0 +1,7 @@
+package io.sleepyhoon.project1.exception;
+
+public class DataDuplicationException extends RuntimeException {
+  public DataDuplicationException(String message) {
+    super(message);
+  }
+}
