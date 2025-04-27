0c4b7ccc7141d681353a52ac324376e618017be9
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-04-27 23:47:24 +0900

    Refactor: 컨트롤러 수정; 사용자정보 노출 최소화

--- src/main/java/io/sleepyhoon/project1/controller/AuthController.java
+++ src/main/java/io/sleepyhoon/project1/controller/AuthController.java
@@ -1,5 +1,6 @@
 package io.sleepyhoon.project1.controller;
 
+import io.sleepyhoon.project1.dto.MemberDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ public class AuthController {
     @GetMapping("/me")
     public Map<String, Object> currentUserInfo(Authentication authentication) {
         Map<String, Object> result = new HashMap<>();
-        result.put("name", authentication.getName());
-        result.put("email", authentication.getPrincipal());
-        result.put("authorities", authentication.getAuthorities());
+
+        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+        result.put("email", memberDetails.getEmail());
+        result.put("username", memberDetails.getUsername());
+        result.put("role", memberDetails.getAuthorities());
+
         return result;
     }
 
